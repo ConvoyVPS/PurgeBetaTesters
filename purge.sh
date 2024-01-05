6297840fc905a45375d4283fc15f8e59d55364c9
leo852 <33802295+leo852@users.noreply.github.com>
2024-01-06 04:34:23 +0900

    Update purge.sh

--- purge.sh
+++ purge.sh
@@ -2,22 +2,23 @@
 
 echo "Fetching VM list from ProxMox..."
 
-# Get a list of all VMs - extract just the IDs
+# Get a list of all VM IDs
 VM_IDS=$(qm list | awk '{if(NR>1)print $1}') # Skipping the first row as it's usually headers
 
 # Loop through each VM ID and check if it's greater than 8 digits
 for VM_ID in $VM_IDS; do
-    # Check if VM_ID has more than 8 digits using string length
+    # Check if VM_ID has more than 8 digits
     if [ ${#VM_ID} -gt 8 ]; then
-        echo "Deleting VM with ID: $VM_ID"
-        # Shutdown the VM if it's running and wait for the operation to complete
-        qm stop "$VM_ID" --skiplock --kill
+        echo "Forcefully stopping VM with ID: $VM_ID..."
+        # Force stop the VM without waiting for a graceful shutdown
+        qm stop $VM_ID --skiplock --forceStop 2>/dev/null
         
         # Wait a moment to ensure the VM is stopped
         sleep 5
         
         # Destroy the VM
-        qm destroy "$VM_ID" --purge
+        echo "Destroying VM with ID: $VM_ID..."
+        qm destroy $VM_ID --force 2>/dev/null
         echo "VM with ID: $VM_ID has been deleted."
     fi
 done
