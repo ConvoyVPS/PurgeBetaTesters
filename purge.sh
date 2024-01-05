4c18df777cba34a41a40ece18a6e852420f4c9a4
leo852 <33802295+leo852@users.noreply.github.com>
2024-01-06 06:04:52 +0900

    Update purge.sh

--- purge.sh
+++ purge.sh
@@ -1,31 +1,61 @@
 #!/bin/bash
 
-echo "Fetching VM list from ProxMox..."
+LOGFILE="/var/log/proxmox_bulk_delete.log"
 
-# Get a list of all VM IDs
-VM_IDS=$(qm list | awk '{if(NR>1)print $1}') # Skipping the first row as it's usually headers
-
-# Loop through each VM ID and check if it's greater than 8 digits
-for VM_ID in $VM_IDS; do
-    # Check if VM_ID has more than 8 digits
-    if [ ${#VM_ID} -gt 8 ]; then
-        echo "Stopping VM with ID: $VM_ID, if it's running..."
-        # Force stop the VM
-        qm stop $VM_ID --skiplock 1>/dev/null
-        
-        # Wait a moment to ensure the VM is stopped
-        sleep 5
-        
-        # Destroy the VM
-        echo "Destroying VM with ID: $VM_ID..."
-        qm destroy $VM_ID 2>&1
-        
-        if [ $? -eq 0 ]; then
-            echo "VM with ID: $VM_ID has been deleted."
-        else
-            echo "An error occurred while attempting to delete VM with ID: $VM_ID"
+# Function to count VMs with IDs longer than 6 digits
+count_vms() {
+    local count=0
+    VM_IDS=$(qm list | awk '{if(NR>1)print $1}') # Skipping the first row as it's usually headers
+    for VM_ID in $VM_IDS; do
+        if [ ${#VM_ID} -gt 6 ]; then
+            ((count++))
         fi
-    fi
-done
+    done
+    echo $count
+}
 
-echo "Bulk deletion of ProxMox VMs with IDs larger than 8 digits completed."
+# Count the VMs to be deleted
+VM_TO_DELETE_COUNT=$(count_vms)
+
+# Display the count and ask for user confirmation
+echo "Found $VM_TO_DELETE_COUNT VMs with IDs longer than 6 digits to be deleted."
+read -p "Do you want to proceed with the deletion? (yes/no): " confirmation
+
+# Check user confirmation
+if [[ $confirmation != "yes" ]]; then
+    echo "Deletion process cancelled by user."
+    exit 0
+fi
+
+# Initialize counters
+VM_DELETED_COUNT=0
+VM_FAILED_COUNT=0
+
+{
+    echo "ProxMox bulk VM deletion process started."
+
+    # Loop through each VM ID and check if it's greater than 8 digits
+    for VM_ID in $VM_IDS; do
+        if [ ${#VM_ID} -gt 6 ]; then
+            echo "Stopping VM with ID: $VM_ID..."
+            qm stop $VM_ID --skiplock
+
+            # Wait a moment to ensure the VM is stopped
+            sleep 5
+
+            # Destroy the VM
+            echo "Destroying VM with ID: $VM_ID..."
+            if qm destroy $VM_ID; then
+                echo "VM with ID: $VM_ID has been successfully deleted."
+                ((VM_DELETED_COUNT++))
+            else
+                echo "Failed to delete VM with ID: $VM_ID."
+                ((VM_FAILED_COUNT++))
+            fi
+        fi
+    done
+
+    echo "Bulk deletion completed."
+    echo "Total VMs Deleted: $VM_DELETED_COUNT"
+    echo "Total VMs Failed: $VM_FAILED_COUNT"
+} 2>&1 | tee -a $LOGFILE    # Output to both the console and the log file
