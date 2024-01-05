3a2a46fb146781483536b9a7bcc89e614a3f8299
leo852 <33802295+leo852@users.noreply.github.com>
2024-01-06 06:07:09 +0900

    Update purge.sh

--- purge.sh
+++ purge.sh
@@ -14,7 +14,7 @@ count_vms() {
     echo $count
 }
 
-# Count the VMs to be deleted
+# Count the VMs to be deleted before asking for confirmation
 VM_TO_DELETE_COUNT=$(count_vms)
 
 # Display the count and ask for user confirmation
@@ -34,17 +34,20 @@ VM_FAILED_COUNT=0
 {
     echo "ProxMox bulk VM deletion process started."
 
-    # Loop through each VM ID and check if it's greater than 8 digits
+    # Re-fetch the VM_IDS to proceed with the confirmation
+    VM_IDS=$(qm list | awk '{if(NR>1)print $1}')
+
+    # Loop through each VM ID and check if it's greater than 6 digits
     for VM_ID in $VM_IDS; do
         if [ ${#VM_ID} -gt 6 ]; then
-            echo "Stopping VM with ID: $VM_ID..."
+            echo "Stopping VM with ID: $VM_ID ..."
             qm stop $VM_ID --skiplock
 
             # Wait a moment to ensure the VM is stopped
             sleep 5
 
             # Destroy the VM
-            echo "Destroying VM with ID: $VM_ID..."
+            echo "Destroying VM with ID: $VM_ID ..."
             if qm destroy $VM_ID; then
                 echo "VM with ID: $VM_ID has been successfully deleted."
                 ((VM_DELETED_COUNT++))
